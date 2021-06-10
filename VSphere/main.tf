90359a0a45b8679cb6690342f7495515fad59f5d
DomiSokol <65492315+DomiSokol@users.noreply.github.com>
2021-06-10 13:34:01 +0200

    Update main.tf

--- VSphere/main.tf
+++ VSphere/main.tf
@@ -1,9 +1,35 @@
-provider "vra"{
-  url = "${var.rul}"
+provider "vra" {
+  insecure = true
+  url = "https://vra8.vdi.sclabs.net"
+  refresh_token = "uhlsrxhilMjgdzAwgxPXOs8Lbe6Pyw6h"
 }
 
-resource "vra_cloud_account_vsphere" "this" {
-  username = 
-  password = 
-  hostname = 
+resource "vra_machine" "this" {
+  name        = "tf-machine"
+  description = "terrafrom test machine"
+  project_id  = data.vra_project.this.id
+  image       = "Ubuntu"
+  flavor      = "Test-Flavor-Small"
+
+  boot_config {
+    content = <<EOF
+#cloud-config
+  users:
+  - default
+  - name: myuser
+    plain_text_passwd: "Test"
+    sudo: ['ALL=(ALL) NOPASSWD:ALL']
+    groups: sudo
+    shell: '/bin/bash'
+    ssh-authorized-keys: |
+      ssh-rsa your-ssh-rsa:
+    - sudo sed -e 's/.*PasswordAuthentication yes.*/PasswordAuthentication no/' -i /etc/ssh/sshd_config
+    - sudo service sshd restart
+EOF
+  }
+
+  disks {
+    block_device_id = vra_block_device.disk1.id
+  }
 }
+
