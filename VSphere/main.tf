7d4456df55e7a8cb7db8656992d8b2d958cf9c24
DomiSokol <65492315+DomiSokol@users.noreply.github.com>
2021-06-10 16:27:07 +0200

    Update main.tf

--- VSphere/main.tf
+++ VSphere/main.tf
@@ -1,32 +1,47 @@
-provider "vra" {
-  insecure = true
-  url = "https://vra8.vdi.sclabs.net"
-  refresh_token = "uhlsrxhilMjgdzAwgxPXOs8Lbe6Pyw6h"
+variable "vm_name" {
+  type        = string
+  description = "VM Name"
 }
 
-resource "vra_machine" "this" {
-  name        = "tf-machine"
-  description = "terrafrom test machine"
-  project_id  = data.vra_project.this.id
-  image       = "Ubuntu"
-  flavor      = "Test-Flavor-Small"
 
-  boot_config {
-    content = <<EOF
-#cloud-config
-  users:
-  - default
-  - name: myuser
-    plain_text_passwd: "Test"
-    sudo: ['ALL=(ALL) NOPASSWD:ALL']
-    groups: sudo
-    shell: '/bin/bash'
-    ssh-authorized-keys: |
-      ssh-rsa your-ssh-rsa:
-    - sudo sed -e 's/.*PasswordAuthentication yes.*/PasswordAuthentication no/' -i /etc/ssh/sshd_config
-    - sudo service sshd restart
-EOF
+provider "vsphere" {
+
+  # If you have a self-signed cert
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "dc" {
+  name = "Noris"
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = "DemoDS"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+
+data "vsphere_network" "network" {
+  name          = "VM Network"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = var.vm_name
+  datastore_id     = "${data.vsphere_datastore.datastore.id}"
+
+
+
+  network_interface {
+    network_id = "${data.vsphere_network.network.id}"
   }
 
-}
+  num_cpus = 1
+  memory   = 1024
+  guest_id = "other3xLinux64Guest"
 
+  disk {
+    label = "disk0"
+    size  = 10
+  }
+  wait_for_guest_net_timeout    = -1
+}
